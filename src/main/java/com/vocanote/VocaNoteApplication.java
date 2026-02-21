3bf6ec8f5b61c0ac2ccb9e1a528bea973ec83152
min <minsulee00@naver.com>
2026-02-21 21:01:31 +0900

    Initial commit

---
+++ src/main/java/com/vocanote/VocaNoteApplication.java
@@ -0,0 +1,13 @@
+package com.vocanote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocaNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VocaNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vocanote/VocaNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vocanote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocaNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
